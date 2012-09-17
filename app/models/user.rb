198fa19e91650b0aa4e13a4bdbe884647232edd1
Jay Fajardo <jay@proudcloud.net>
2012-09-17 22:26:46 +0800

    restrict ability to register as a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
