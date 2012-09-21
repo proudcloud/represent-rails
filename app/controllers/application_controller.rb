4ad053bc0c3c52df84a43cdb68a3cc316a483338
Victor Solis <hvictorsolis@gmail.com>
2012-09-21 13:41:58 +0800

    Scrollable "Add something" form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  rescue_from ActionDispatch::Session::SessionRestoreError do |exception|
-    reset_session
-    redirect_to root_path
-  end
-
 end
