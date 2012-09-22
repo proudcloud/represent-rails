bd5473e923dd07cd375cab1a5940e6d8cc8f77bd
Jay Fajardo <jay@proudcloud.net>
2012-09-22 23:55:31 +0800

    add map.js to config.assets.precompile directive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ RepresentPh::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w(map.js)
+  config.assets.precompile += %w(bootstrap-typeahead.js)
+  config.assets.precompile += %w(label.js)
+  config.assets.precompile += %w(form.js)
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
