40dc8f93df576b1fd0caccb6e854151344ba5c16
Jay Fajardo <jay@proudcloud.net>
2012-10-05 17:40:25 +0800

    reference named scope dynamically instead of through type criteria so we can have variable orders and scopes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,7 +27,7 @@ class PlacesController < InheritedResources::Base
     gon.places = []
 
     TYPES.each do |type|
-      Place.where(approved: 1, type: type[1]).order_by(:title.asc).each do |value|
+      Place.send(type[1].to_sym).where(approved: 1).order_by(:title.asc).each do |value|
         place = ["#{value.title}", 
                 "#{value.type}",
                 "#{value.lat}",
