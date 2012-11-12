d97780276dbb801ca92333e0293767faacc7fa4d
Victor Solis <hvictorsolis@gmail.com>
2012-11-12 16:00:38 +0800

    Fix for user deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to users_url, :notice => "User created."
+      redirect_to users_url, :notice => "User created"
     else
       render :action => 'new'
     end
@@ -43,6 +43,15 @@ class UsersController < ApplicationController
 
     end
 
+  end
+
+  def destroy
+    if User.count == 1
+      redirect_to users_path
+      flash[:error] = "Can't delete last user"
+    else
+      destroy!
+    end
   end
 
 end
